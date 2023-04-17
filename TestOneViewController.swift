ee3a9e87c8bca5331e51d4ccd8cfdeca1c8381b3
polosa00 <i@apolochanin.ru>
2023-04-17 18:25:24 +0300

    Set Test One VC

---
+++ TestOneViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TestOneViewController.swift
+//  test1
+//
+//  Created by Александр Полочанин on 17.04.23.
+//
+
+import UIKit
+
+class TestOneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
